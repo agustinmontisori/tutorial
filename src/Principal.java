8834dc71c109189e140e7b3929f0b669c74f0d74
MONTISORI AGUSTIN <agustinmontisori@outlook.com>
2020-09-13 17:03:25 -0300

    Primer commit de un proyecto desde Eclipse

---
+++ src/Principal.java
@@ -0,0 +1,27 @@
+
+/*
+Ejercicio 1
+
+Mostrar por consola los 10 primeros números enteros y los 10 primeros números impares
+ */
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int cant = 0;
+		
+		for (int i = 0; i <10; i++) {
+			System.out.println(i+1);			
+		}
+
+		System.out.println();
+		
+		for (int i = 1; i <20; i+=2) {
+			cant = cant+1;
+			System.out.println("el impar n°: "+cant+" es: "+i);			
+		}
+		
+	}
+
+}
